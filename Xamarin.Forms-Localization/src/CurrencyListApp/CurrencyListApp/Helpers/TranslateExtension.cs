5e8877efcc34ceab40386208bfca53676f44100d
s v <chofexx@gmail.com>
2020-11-26 06:53:00 +0200

    Adding Translation Helper

---
+++ Xamarin.Forms-Localization/src/CurrencyListApp/CurrencyListApp/Helpers/TranslateExtension.cs
@@ -0,0 +1,21 @@
+﻿using CurrencyListApp.Resouces;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CurrencyListApp.Helpers
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return null;
+
+            return AppResources.ResourceManager.GetString(Text, AppResources.Culture);
+        }
+    }
+}
