5a7e5d45da0b6df5ef5114c83be8d43dba144029
s v <chofexx@gmail.com>
2020-11-27 07:42:41 +0200

    Adding Effect

---
+++ Extending-Xamarin.Forms/src/GBMExtending/GBMExtending.UWP/FocusEffect.cs
@@ -0,0 +1,33 @@
+﻿using GBMExtending.Effects;
+using System;
+using System.Diagnostics;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ResolutionGroupName("Pluralsight")]
+[assembly: ExportEffect(typeof(FocusEffect), nameof(FocusEffect))]
+namespace GBMExtending.UWP
+{
+    public class FocusEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                (Control as Windows.UI.Xaml.Controls.Control).Background = new Windows.UI.Xaml.Media.SolidColorBrush(Colors.Azure);
+                (Control as FormsTextBox).BackgroundFocusBrush = new SolidColorBrush(Colors.White);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Extending-Xamarin.Forms/src/GBMExtending/GBMExtending/Effects/FocusEffect.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace GBMExtending.Effects
+{
+    public class FocusEffect : RoutingEffect
+    {
+        public FocusEffect()
+            : base($"Pluralsight.{nameof(FocusEffect)}")
+        {
+
+        }
+    }
+}
