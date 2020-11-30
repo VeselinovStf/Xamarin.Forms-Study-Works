1fc3df1242eca61df598464b1c2dd31e27526b89
s v <chofexx@gmail.com>
2020-11-30 07:18:44 +0200

    Adding Request runtime location permissions

--- Google-Maps-in-Xamarin.Forms/src/SalesApp.Android/MainActivity.cs
+++ Google-Maps-in-Xamarin.Forms/src/SalesApp.Android/MainActivity.cs
@@ -6,12 +6,21 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android;
 
 namespace SalesApp.Droid
 {
     [Activity(Label = "Sales App", Icon = "@drawable/logo", Theme = "@style/splashscreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        const int RequestLocationId = 0;
+
+        readonly string[] LocationPermissions =
+        {
+             Manifest.Permission.AccessCoarseLocation,
+             Manifest.Permission.AccessFineLocation
+        };
+
         protected override void OnCreate(Bundle bundle)
         {
             base.Window.RequestFeature(WindowFeatures.ActionBar);
@@ -23,6 +32,7 @@ namespace SalesApp.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+         
             Xamarin.FormsGoogleMaps.Init(this, bundle); //Initialize GoogleMaps here
 
             UserDialogs.Init(this);
@@ -30,9 +40,44 @@ namespace SalesApp.Droid
             LoadApplication(new App());
         }
 
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            if ((int)Build.VERSION.SdkInt >= 23)
+            {
+                if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+                {
+                    RequestPermissions(LocationPermissions, RequestLocationId);
+                }
+                else
+                {
+                    // Permissions already granted - display a message.
+                }
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
         {
-            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (requestCode == RequestLocationId)
+            {
+                if ((grantResults.Length == 1) && (grantResults[0] == (int)Permission.Granted)) 
+                {
+                    // Permissions granted - display a message.
+                }
+
+                else
+                {
+                    // Permissions denied - display a message.
+                }
+
+            }
+            else
+            {
+                Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+
+            
         }
     }
 }
